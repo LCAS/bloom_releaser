930044e6523d2251ac9e0cd61a09071b20e15cec
Marc Hanheide <marc@hanheide.de>
2017-05-23 18:50:07 +0100

    bloom-rerelease added

--- releaser.sh
+++ releaser.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+DIR=$(dirname $(readlink -f $0))
+
 ls ~/.git-credentials
 
 echo "repo=$REPOSITORY"
 echo "distro=$ROSDISTRO"
 
-git clone https://gitsvn-nt.oru.se/iliad/software/private_sandbox.git
+#git clone https://gitsvn-nt.oru.se/iliad/software/private_sandbox.git
+
+$DIR/bloom-rerelease.py --bump $BUMP $REPOSITORY $ROSDISTRO 
+bloom-release -y --no-web -t $ROSDISTRO  -r $ROSDISTRO $REPOSITORY
